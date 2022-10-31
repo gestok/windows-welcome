0f9930c8da71b3ead35c92fa4313601736b06769
gestok <giorgos.cho@hotmail.com>
2022-10-31 15:18:46 +0200

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import AppendHead from 'react-append-head';
 import LoginScreen from './components/LoginScreen.js';
 import 'normalize.css';
 import './style.css';
@@ -7,13 +6,6 @@ import './style.css';
 export default function App() {
   return (
     <>
-      <AppendHead>
-        <link
-          name="segoe-ui"
-          href="https://fonts.cdnfonts.com/css/segoe-ui-4"
-          rel="stylesheet"
-        />
-      </AppendHead>
       <LoginScreen />
     </>
   );
